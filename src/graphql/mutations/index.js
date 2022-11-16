0b9f3e96ead0a7710ca3a9acfe2ce7bd399f83ec
Thien Nguyen <jasonnguyen141091@gmail.com>
2022-11-16 21:25:31 +0700

    update to add API turn

--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -1,7 +1,9 @@
 const { addAuthor, updateAuthor, deleteAuthor } = require('./AuthorMutation');
-const { createPost, updatePost, deletePost} = require('./PostMutation')
+const { createPost, updatePost, deletePost} = require('./PostMutation');
+const { addTurn } = require('./TurnMutation');
 
 module.exports = {
+  addTurn,
   addAuthor,
   updateAuthor,
   deleteAuthor,
